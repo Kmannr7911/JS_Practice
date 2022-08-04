18e210b846be6ee23d5842e2fd2f3f0ae268d9d2
ConnorK26 <cklingmnn5197@gmail.com>
2022-08-04 16:45:16 -0400

    JS_Practice

---
+++ index.js
@@ -0,0 +1,15 @@
+let count = 5;
+count = count + 1;
+console.log(count);
+
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints= bonusPoints + 100;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 25;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 70;
+console.log(bonusPoints);
