c19963446d38b02a5eeadbeba3a8185d4ddb44b9
ConnorK26 <cklingmnn5197@gmail.com>
2022-09-04 13:52:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-let count = 5;
-count = count + 1;
-console.log(count);
+// let count = 5;
+// count = count + 1;
+// console.log(count);
 
-let bonusPoints = 50;
-console.log(bonusPoints);
+// let bonusPoints = 50;
+// console.log(bonusPoints);
 
-bonusPoints= bonusPoints + 100;
-console.log(bonusPoints);
+// bonusPoints= bonusPoints + 100;
+// console.log(bonusPoints);
 
-bonusPoints = bonusPoints - 25;
-console.log(bonusPoints);
+// bonusPoints = bonusPoints - 25;
+// console.log(bonusPoints);
 
-bonusPoints = bonusPoints + 70;
-console.log(bonusPoints);
+// bonusPoints = bonusPoints + 70;
+// console.log(bonusPoints);
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function logLapTime() {
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
+}
